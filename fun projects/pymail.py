26b5541e7858e7dadbf77cb0022a5dde87db560f
dKISS <150686007+LeinadSiK@users.noreply.github.com>
2024-05-24 10:12:22 +0200

    Update pymail.py

--- fun projects/pymail.py
+++ fun projects/pymail.py
@@ -1,13 +1,13 @@
-# lgip zssb qejn jstq
+
 
 from email.message import EmailMessage
 from email.mime.image import MIMEImage
 import ssl
 import smtplib
 
-sender = 'leinad.sik@gmail.com'
-receiver = 'leinad.sik@gmail.com'
-app_password = 'lgip zssb qejn jstq'
+sender = 'sender email'
+receiver = 'receiver email'
+app_password = 'your password'
 
 subject = input('Subject: ')
 body = input('Body: ')
@@ -21,7 +21,7 @@ message.set_content(body)
 attach_image = input('Attach image? (y/n): ')
 if attach_image.startswith('y'):
 
-    with open(r'C:\Users\dkiss\Desktop\me.jpg', 'rb') as img:
+    with open(r'your directory', 'rb') as img:
         img_data = img.read()
     message.add_attachment(img_data, maintype='image', subtype='jpeg', filename='image.jpg')
 
